3e23259183891435e379a02b8fb3191dfedabe26
HUGUETA <HUGUETA@172.30.44.172>
2015-12-11 09:54:46 +0000

    added some classes about the game board.

--- Scrabble_Client/src/game_model/Scrabble.java
+++ Scrabble_Client/src/game_model/Scrabble.java
@@ -5,10 +5,31 @@
  */
 package game_model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author HUGUETA
  */
 public class Scrabble {
+    public final Board board = new Board();
+    public ArrayList<Player> players;
+    public Bag bag = new Bag();
+    
+    public Scrabble(int playersNumber)
+    {
+        players = new ArrayList<Player>(playersNumber);
+    }
+    
+    public void run(){
+        
+    }
+    
+    public void firstLetters()
+    {
+        for(Player p : players)
+            for(int i=0; i<7,i++)
+                p.addLetter(bag.getNext());
+    }
     
 }
